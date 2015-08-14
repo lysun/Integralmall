40c33cb6073ba0bc747a033a242a3174697c54b2
sunli <sunli_200704@126.com>
2015-08-14 12:35:20 +0800

    删除文件

--- core/src/main/java/com/doublev2v/integralmall/order/OrderMerchandiseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.doublev2v.integralmall.order;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface OrderMerchandiseRepository extends PagingAndSortingRepository<OrderMerchandise, String>{
-
-	public OrderMerchandise findByCouponCode(String couponCode);
-}
--- core/src/main/java/com/doublev2v/integralmall/user/UserTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.doublev2v.integralmall.user;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface UserTokenRepository extends PagingAndSortingRepository<UserToken, String>{
-
-	public UserToken findByToken(String token);
-}
