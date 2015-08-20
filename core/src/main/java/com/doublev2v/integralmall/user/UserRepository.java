c6e4b989f9d6f1d915273097b668540450a27b49
sunli <sunli_200704@126.com>
2015-08-20 18:34:15 +0800

    删除文件

--- core/src/main/java/com/doublev2v/integralmall/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.doublev2v.integralmall.user;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface UserRepository extends PagingAndSortingRepository<User, String>{
-
-}
