f29ec1150a7afca02add3a82476f127db5c7f3c7
sunli <sunli_200704@126.com>
2015-09-07 15:52:11 +0800

    测试

--- web/src/test/java/com/doublev2v/ServiceTest.java
+++ web/src/test/java/com/doublev2v/ServiceTest.java
@@ -1,7 +1,5 @@
 package com.doublev2v;
 
-import java.util.List;
-
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.junit.Before;
@@ -11,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.doublev2v.integralmall.auth.menu.Menu;
 import com.doublev2v.integralmall.auth.menu.MenuService;
 import com.doublev2v.integralmall.order.IntegralOrderRepository;
 
