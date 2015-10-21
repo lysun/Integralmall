9c58d4c2c029013be3d0c904d59eaac12014d96a
TianmingYi <yzdytm@163.com>
2015-10-21 21:15:25 +0800

    完成高定列表查询

---
+++ domain/src/main/java/com/doublev2v/integralmall/designerCraft/DesignerCraft.java
@@ -0,0 +1,19 @@
+package com.doublev2v.integralmall.designerCraft;
+
+public class DesignerCraft {
+
+	private String id;
+	private String picUrl;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPicUrl() {
+		return picUrl;
+	}
+	public void setPicUrl(String picUrl) {
+		this.picUrl = picUrl;
+	}
+}
