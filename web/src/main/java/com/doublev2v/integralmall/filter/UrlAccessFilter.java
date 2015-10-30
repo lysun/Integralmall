51add470caf89ce6ce5d77aa1ab6b447ade746a9
sunli <sunli_200704@126.com>
2015-10-30 11:16:01 +0800

    shiro跳转到之前被拦截的页面

--- web/src/main/java/com/doublev2v/integralmall/filter/UrlAccessFilter.java
+++ web/src/main/java/com/doublev2v/integralmall/filter/UrlAccessFilter.java
@@ -5,11 +5,13 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.PathMatchingFilter;
 
 import com.doublev2v.integralmall.auth.permission.UrlAccessDefinition;
@@ -37,16 +39,19 @@ public class UrlAccessFilter extends PathMatchingFilter{
 	@Override
 	protected boolean preHandle(ServletRequest request, ServletResponse response)
 			throws Exception {
+		Subject subject = SecurityUtils.getSubject();
 		HttpServletRequest req=(HttpServletRequest)request;
 		HttpServletResponse res=(HttpServletResponse)response;
+		HttpSession session=req.getSession();
 		String requestUrl=req.getServletPath();
+		if(!subject.isAuthenticated()){//如果用户没登录跳转到登录页面，并在session中记录登陆成功后需要重定向的请求
+			session.setAttribute("successUrl", requestUrl);
+			res.sendRedirect(getLoginUrl(req));
+			return false;
+		}
 		Iterable<UrlAccessDefinition> list=urlAccessDefinitionManager.findAll();
 		if(list!=null){
 			for(UrlAccessDefinition u:list){//遍历所有url权限数据，如果url匹配当前请求，则判断当前用户是否拥有此url所具备的角色或权限
-				if(!SecurityUtils.getSubject().isAuthenticated()){
-					res.sendRedirect(getLoginUrl(req));
-					return false;
-				}
 				if(pathMatcher.matches(u.getUrl(), requestUrl)){					
 					boolean role=true;
 					if(StringUtils.isNotBlank(u.getRole())){
@@ -70,4 +75,5 @@ public class UrlAccessFilter extends PathMatchingFilter{
 	private String getLoginUrl(HttpServletRequest req){
 		return req.getContextPath()+"/login";
 	}
+	
 }  
